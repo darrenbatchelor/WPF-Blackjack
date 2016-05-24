110f0ad7cc7e4c29413a95892e301db097e2962d
darrenbatchelor <dbatch76@hotmail.com>
2016-05-23 22:51:15 -0700

    Add files via upload
    Initial commit

---
+++ Blackjack.Model/Blackjack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blackjack.Model
+{
+    class Blackjack : CardGame
+    {
+        private string _name;
+        public override string Name  
+        {
+            get 
+            { 
+                return _name; 
+            } 
+            set 
+            {
+                if (String.IsNullOrEmpty(_name))
+                {
+                    _name = "Blackjack";
+                }
+            } 
+        }
+
+        public override int DeckSize { get; set; }
+        public override bool IsMultiDeck { get; set; }
+    }
+}
---
+++ Blackjack.Model/CardGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blackjack.Model
+{
+    abstract class CardGame : Game
+    {
+        public override string Name { get; set; }
+        public abstract int DeckSize { get; set; }
+        public abstract bool IsMultiDeck { get; set; }
+    }
+}
